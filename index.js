bfcb74db790e13f3998116f55e2b690c6d2ed036
victorguy123 <149330457+victorguy123@users.noreply.github.com>
2026-08-26 13:15:10 +0100

    add new cmds

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.command("/bing-bong-bot-here-world", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `Hello, World! How are you today?` });
 });
 
-app.command("/bing-bong-bot-here-hello", async ({ command, ack, respond }) => {
+app.command("/bing-bong-bot-here-im-a-bot-too", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `Snap! We're both bots! 01001100 01100101 01110100 01110011 00100000 01110100 01100001 01101100 01101011 00100000 01110011 01100101 01100011 01110010 01100101 01110100 01101100 01111001 00100000 00111011 00110000` });
 });
 
 (async () => {
